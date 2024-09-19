05698aaeffc9383615fd57065babd122519be250
fschafr <fabian.schafroth@mercedes-benz.com>
2024-09-19 17:04:28 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.3.3"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+
+	// Spring Cloud Stream
+	implementation(platform("com.azure.spring:spring-cloud-azure-dependencies:5.16.0"))
+	implementation("com.azure.spring:spring-cloud-azure-stream-binder-servicebus")
+	implementation("org.springframework.cloud:spring-cloud-stream:4.1.3")
+	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder:4.1.3")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/example/demo/ConsumerConfig.kt
@@ -0,0 +1,19 @@
+package com.example.demo
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.function.Consumer
+
+@Configuration
+class ConsumerConfig {
+
+    @Bean
+    fun process() = Consumer<Any> { msg ->
+        println(msg)
+    }
+
+
+    @Bean
+    fun myObjectMapper() = ObjectMapper()
+}
